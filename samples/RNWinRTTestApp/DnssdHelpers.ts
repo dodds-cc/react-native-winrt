db00d3d23bc89423b3f71859f8b9cfa5f53c8c8f
Thomas Dodds <thomas@creativelycommon.co.uk>
2023-08-29 21:35:37 +0100

    change service type and delay time

--- samples/RNWinRTTestApp/DnssdHelpers.ts
+++ samples/RNWinRTTestApp/DnssdHelpers.ts
@@ -52,7 +52,7 @@ export class DnssdLookupHelper {
   private static readonly PROTOCOL_GUID =
     "{4526e8c1-8aac-4153-9b16-55e86ada0e54}"; // Protocol type for DNS-SD.
   private static readonly DOMAIN = "local"; // Example: "local" in "fooDevice._ssh._tcp.local".
-  private static readonly SERVICE_NAME = "_ipp._tcp"; // Change as needed. Example: "_ipp._tcp" in "fooDevice._ipp._tcp.local".
+  private static readonly SERVICE_NAME = "_http._tcp"; // Change as needed. Example: "_ipp._tcp" in "fooDevice._ipp._tcp.local".
 
   private static readonly aqsQuery = `System.Devices.AepService.ProtocolId:=${DnssdLookupHelper.PROTOCOL_GUID} 
                                         AND System.Devices.Dnssd.Domain:=${DnssdLookupHelper.DOMAIN} 
@@ -143,7 +143,7 @@ export class DnssdLookupHelper {
     discoveredServices: IDnssdServiceInstance[],
   ) => void = () => {};
 
-  private DEVICE_INFORMATION_PARSING_DELAY: number = 500;
+  private DEVICE_INFORMATION_PARSING_DELAY: number = 2000;
 
   /**
    * Starts listening for devices and subscribes to the `added` and `updated` events.
